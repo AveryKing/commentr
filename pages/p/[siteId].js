bcfd91b94f20a7d4506931bd5c9f7195ce420af5
Avery King <avery.n.king@wmich.edu>
2022-03-21 19:00:57 -0400

    submit handler added

--- pages/p/[siteId].js
+++ pages/p/[siteId].js
@@ -1,8 +1,10 @@
 import {useRouter} from 'next/router';
-import {useRef} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import {Box, FormControl, FormLabel, Input, Button} from '@chakra-ui/react'
 import { getAllFeedback, getAllSites } from '@/lib/db-admin';
 import {useAuth} from '@/lib/auth';
+import {createFeedback} from '@/lib/db';
+import Feedback from "@/components/Feedback";
 
 export async function getStaticProps(context) {
     const siteId = context.params.siteId
@@ -31,9 +33,33 @@ export async function getStaticPaths() {
 }
 
 export default function FeedbackPage({initialFeedback}) {
+    const router = useRouter();
     const auth = useAuth();
     const inputElement = useRef(null);
+    const [allFeedback, setAllFeedback] = useState([]);
 
+    useEffect(() => {
+        setAllFeedback(initialFeedback);
+    }, [initialFeedback])
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const newFeedback = {
+            author: auth.user.name,
+            authorId: auth.user.uid,
+            siteId: router.query.siteId,
+            text: inputElement.current.value,
+            createdAt: new Date().toISOString(),
+            provider: auth.user.provider,
+            status: 'pending'
+        }
+
+        inputElement.current.value = '';
+        setAllFeedback((currentFeedback) => [newFeedback, ...currentFeedback]);
+        createFeedback(newFeedback)
+            .then(r => null);
+    }
     return (
         <Box
             display='flex'
@@ -43,7 +69,7 @@ export default function FeedbackPage({initialFeedback}) {
             margin='0 auto'
             >
             {auth.user && (
-                <Box as='form'>
+                <Box as='form' onSubmit={onSubmit}>
                     <FormControl my={8}>
                         <FormLabel htmlFor='comment'>Comment</FormLabel>
                         <Input ref={inputElement} id='comment' placeholder='Leave a comment'/>
