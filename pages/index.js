22a86ff514bc496008eab07adb216ffec4bd4c7c
Avery King <avery.n.king@wmich.edu>
2022-03-21 19:27:52 -0400

    ``EmptyState`` component added

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import {useAuth} from '@/lib/auth';
 import {Button} from "@chakra-ui/react";
+import DashboardShell from "@/components/DashboardShell";
 
 export default function Index() {
     const auth = useAuth();
 
     return auth.user ? (
-        <div>
+        <DashboardShell>
             <p>Email: {auth.user.email}</p>
             <button onClick={(e) => auth.signOut()}>
                 Sign Out
             </button>
-        </div>
+        </DashboardShell>
     ) : (
         <>
             <Button onClick={(e) => auth.signInWithGithub()}>
