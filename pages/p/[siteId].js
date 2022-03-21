02c6627e6609af551ef33eaf7f57f0e2e30a0267
Avery King <avery.n.king@wmich.edu>
2022-03-21 18:46:41 -0400

    comment form created

--- pages/p/[siteId].js
+++ pages/p/[siteId].js
@@ -1,6 +1,8 @@
 import {useRouter} from 'next/router';
-import Box from '@chakra-ui/react';
-import { getAllFeedback, getAllSites } from '../../lib/db-admin';
+import {useRef} from 'react';
+import {Box, FormControl, FormLabel, Input, Button} from '@chakra-ui/react'
+import { getAllFeedback, getAllSites } from '@/lib/db-admin';
+import {useAuth} from '@/lib/auth';
 
 export async function getStaticProps(context) {
     const siteId = context.params.siteId
@@ -28,8 +30,32 @@ export async function getStaticPaths() {
     }
 }
 
-export default function FeedbackPage() {
-    const router = useRouter();
+export default function FeedbackPage({initialFeedback}) {
+    const auth = useAuth();
+    const inputElement = useRef(null);
 
-    return <Box>{router.query.siteId}</Box>
+    return (
+        <Box
+            display='flex'
+            flexDirection='column'
+            width='full'
+            maxWidth='700px'
+            margin='0 auto'
+            >
+            {auth.user && (
+                <Box as='form'>
+                    <FormControl my={8}>
+                        <FormLabel htmlFor='comment'>Comment</FormLabel>
+                        <Input ref={inputElement} id='comment' placeholder='Leave a comment'/>
+                        <Button mt={4} type='submit' fontWeight='medium'>
+                            Add Comment
+                        </Button>
+                    </FormControl>
+                </Box>
+            )}
+            {initialFeedback && initialFeedback.map((feedback) => (
+                <Feedback key={feedback.id} {...feedback} />
+            ))}
+        </Box>
+    )
 }
