92639b8d448143009cab767a3bca2357607bbe0a
Avery King <avery.n.king@wmich.edu>
2022-03-21 04:33:06 -0400

    Google sign in added to auth hook

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,25 @@
 import {useAuth} from '../lib/auth';
 
-export default function Index () {
-  const auth = useAuth();
+export default function Index() {
+    const auth = useAuth();
 
-  return auth.user ? (
-      <div>
-        <p>Email: {auth.user.email}</p>
-        <button onClick={(e) => auth.signOut()}>
-          Sign Out
-        </button>
-      </div>
-  ) : (
-      <button onClick={(e) => auth.signInWithGithub()}>
-        Sign in
-      </button>
-  )
+    return auth.user ? (
+        <div>
+            <p>Email: {auth.user.email}</p>
+            <button onClick={(e) => auth.signOut()}>
+                Sign Out
+            </button>
+        </div>
+    ) : (
+        <>
+            <button onClick={(e) => auth.signInWithGithub()}>
+                Sign in With GitHub
+            </button>
+            <button onClick={(e) => auth.signInWithGoogle()}>
+                Sign in With Google
+            </button>
+
+        </>
+
+    )
 }
