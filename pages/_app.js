ab40791da16bc44084a4f116ade584b5bdde498e
Avery King <avery.n.king@wmich.edu>
2022-03-21 11:53:09 -0400

    chakra provider added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import {AuthProvider} from "../lib/auth";
+import {ChakraProvider} from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
   return (
+      <ChakraProvider>
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
+      </ChakraProvider>
   )
 }
 
