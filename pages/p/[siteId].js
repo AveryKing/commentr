303f94df10d011fac0d78a3e0f4205954bdc8b9b
Avery King <avery.n.king@wmich.edu>
2022-03-21 13:10:50 -0400

    static props/paths added

--- pages/p/[siteId].js
+++ pages/p/[siteId].js
@@ -1,5 +1,32 @@
 import {useRouter} from 'next/router';
 import Box from '@chakra-ui/react';
+import { getAllFeedback, getAllSites } from '../../lib/db-admin';
+
+export async function getStaticProps(context) {
+    const siteId = context.params.siteId
+    const { feedback } = await getAllFeedback(siteId)
+
+    return {
+        props: {
+            initialFeedback: feedback,
+        },
+        revalidate: 1,
+    }
+}
+
+export async function getStaticPaths() {
+    const { sites } = await getAllSites()
+    const paths = sites.map((site) => ({
+        params: {
+            siteId: site.id.toString(),
+        },
+    }))
+
+    return {
+        paths,
+        fallback: true,
+    }
+}
 
 export default function FeedbackPage() {
     const router = useRouter();
